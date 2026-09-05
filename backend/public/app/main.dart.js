6ce6b2d33a1a987ff3693d2bf35745188e4096f0
PouriaEsmaeili <p.esmaeili.b@gmail.com>
2026-09-05 13:54:26 +0330

    rebuild flutter web with app base path

--- backend/public/app/main.dart.js
+++ backend/public/app/main.dart.js
@@ -18842,7 +18842,8 @@ k=e}}l=f}m=h}}if(j){if(n)p=o
 else{j=s?q:b
 o=(j==null?p.a(j):j).b
 p=o}A.ck(p)
-p=new A.ab(Math.max(A.it(m),A.it(k)),Math.max(A.it(l),p))
+a=new A.ab(Math.max(A.it(m),A.it(k)),Math.max(A.it(l),p))
+p=a
 break A}p=d}return p},
 b_Y(a,b,c,d,e,f,g,h,i){var s,r=null,q=A.a9(),p=J.aN9(4,t.iy)
 for(s=0;s<4;++s)p[s]=new A.G7(r,B.aA,B.a_,new A.jc(1),r,r,r,r,B.aB,r)
@@ -108359,4 +108360,3 @@ return}var s=document.scripts
 function onLoad(b){for(var q=0;q<s.length;++q){s[q].removeEventListener("load",onLoad,false)}a(b.target)}for(var r=0;r<s.length;++r){s[r].addEventListener("load",onLoad,false)}})(function(a){v.currentScript=a
 var s=A.aGg
 if(typeof dartMainRunner==="function"){dartMainRunner(s,[])}else{s([])}})})()
-//# sourceMappingURL=main.dart.js.map
