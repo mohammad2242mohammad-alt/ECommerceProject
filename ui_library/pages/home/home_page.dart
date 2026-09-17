8c8bb2b1ad3dc22b50ad640462420275f8a24b5a
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 00:15:28 +0330

    Build configurable Home page from reusable sections

--- ui_library/pages/home/home_page.dart
+++ ui_library/pages/home/home_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class GenericSection extends StatelessWidget {
-  final String name;
-  const GenericSection({super.key, required this.name});
+import '../../layouts/layout_engine.dart';
+
+/// Configurable Home page. Pass sections in the desired visual order.
+class HomePage extends StatelessWidget {
+  final List<LayoutSection> sections;
+  final EdgeInsetsGeometry padding;
+
+  const HomePage({
+    super.key,
+    required this.sections,
+    this.padding = EdgeInsets.zero,
+  });
+
   @override
-  Widget build(BuildContext context) => Padding(padding: const EdgeInsets.all(16), child: Text(name));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConfigurableLayout(
+        sections: sections,
+        padding: padding,
+      ),
+    );
+  }
 }
