140ccc6ba0a669dcc4e56e462cba8ef4a9851559
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 00:15:43 +0330

    Build configurable Product Detail page

--- ui_library/pages/product_detail/product_detail_page.dart
+++ ui_library/pages/product_detail/product_detail_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class GenericSection extends StatelessWidget {
-  final String name;
-  const GenericSection({super.key, required this.name});
+import '../../layouts/layout_engine.dart';
+
+/// Configurable Product Detail page. Product state and actions stay outside.
+class ProductDetailPage extends StatelessWidget {
+  final List<LayoutSection> sections;
+  final EdgeInsetsGeometry padding;
+
+  const ProductDetailPage({
+    super.key,
+    required this.sections,
+    this.padding = EdgeInsets.zero,
+  });
+
   @override
-  Widget build(BuildContext context) => Padding(padding: const EdgeInsets.all(16), child: Text(name));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConfigurableLayout(
+        sections: sections,
+        padding: padding,
+      ),
+    );
+  }
 }
