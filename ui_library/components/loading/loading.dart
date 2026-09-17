655c8f18a83d791ad0e41ac1b70f406f835af41b
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 00:06:27 +0330

    Build reusable loading component

--- ui_library/components/loading/loading.dart
+++ ui_library/components/loading/loading.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
-class GenericSection extends StatelessWidget {
-  final String name;
-  const GenericSection({super.key, required this.name});
+class LoadingComponent extends StatelessWidget {
+  final String? message;
+  final double? size;
+  final EdgeInsetsGeometry padding;
+
+  const LoadingComponent({
+    super.key,
+    this.message,
+    this.size,
+    this.padding = const EdgeInsets.all(24),
+  });
+
   @override
-  Widget build(BuildContext context) => Padding(padding: const EdgeInsets.all(16), child: Text(name));
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Padding(
+        padding: padding,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: size,
+                height: size,
+                child: const CircularProgressIndicator(),
+              ),
+              if (message != null) ...[
+                const SizedBox(height: 12),
+                Text(
+                  message!,
+                  textAlign: TextAlign.center,
+                  style: theme.textTheme.bodyMedium,
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
