6f0a48bb1cda407d6235655daeddc3d4f24d5723
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 00:16:09 +0330

    Build configurable Orders page

--- ui_library/pages/orders/orders_page.dart
+++ ui_library/pages/orders/orders_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class GenericSection extends StatelessWidget {
-  final String name;
-  const GenericSection({super.key, required this.name});
+import '../../layouts/layout_engine.dart';
+
+/// Configurable Orders page. Order state and filtering stay outside.
+class OrdersPage extends StatelessWidget {
+  final List<LayoutSection> sections;
+  final EdgeInsetsGeometry padding;
+
+  const OrdersPage({
+    super.key,
+    required this.sections,
+    this.padding = EdgeInsets.zero,
+  });
+
   @override
-  Widget build(BuildContext context) => Padding(padding: const EdgeInsets.all(16), child: Text(name));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConfigurableLayout(
+        sections: sections,
+        padding: padding,
+      ),
+    );
+  }
 }
