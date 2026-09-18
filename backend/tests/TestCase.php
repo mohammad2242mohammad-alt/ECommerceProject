ef3c5cbdd5e968864eb7a928c88525dff9187a67
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 11:44:28 +0330

    test: enable isolated database refreshes for feature tests

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;\nuse Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
