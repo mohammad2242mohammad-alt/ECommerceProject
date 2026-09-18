2696fa28bac7aac6b219a235ee5ca654342b2943
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-18 13:50:29 +0330

    refactor: build payment result from reusable UI

--- mobile/lib/features/checkout/screens/payment_result_screen.dart
+++ mobile/lib/features/checkout/screens/payment_result_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../core/routes/app_routes.dart';
 import '../../../data/models/order_model.dart';
 import '../../../shared/widgets/store_widgets.dart';
+import '../widgets/payment_result_ui_components.dart';
 
 
 class PaymentResultScreen extends StatelessWidget {
@@ -43,74 +44,7 @@ class PaymentResultScreen extends StatelessWidget {
             children: [
 
 
-              Icon(
-
-                paid
-                    ? Icons.check_circle_outline
-                    : Icons.error_outline,
-
-
-                size:
-                    90,
-
-
-                color:
-                    paid
-                        ? Colors.green
-                        : Colors.red,
-              ),
-
-
-
-              const SizedBox(
-                height:
-                    16,
-              ),
-
-
-
-              Text(
-
-                paid
-                    ? 'پرداخت با موفقیت انجام شد'
-                    : 'پرداخت ناموفق بود',
-
-
-                style:
-                    Theme.of(context)
-                        .textTheme
-                        .titleLarge
-                        ?.copyWith(
-                          fontWeight:
-                              FontWeight.bold,
-                        ),
-              ),
-
-
-
-              const SizedBox(
-                height:
-                    10,
-              ),
-
-
-
-              Text(
-                'شماره سفارش: ${order.orderNumber}',
-              ),
-
-
-
-              const SizedBox(
-                height:
-                    6,
-              ),
-
-
-
-              Text(
-                'مبلغ: ${money(order.total)} تومان',
-              ),
+              PaymentResultCard(order: order, paid: paid),
 
 
 
@@ -121,54 +55,11 @@ class PaymentResultScreen extends StatelessWidget {
 
 
 
-              FilledButton(
-
-                onPressed: () {
-
-                  Navigator.pushNamedAndRemoveUntil(
-
-                    context,
-
-                    AppRoutes.orderDetail,
-
-                    ModalRoute.withName(
-                      AppRoutes.home,
-                    ),
-
-                    arguments:
-                        order.id,
-                  );
-                },
-
-
-                child:
-                    const Text(
-                  'مشاهده سفارش',
-                ),
-              ),
-
-
-
-              TextButton(
-
-                onPressed: () {
-
-                  Navigator.pushNamedAndRemoveUntil(
-
-                    context,
-
-                    AppRoutes.home,
-
-                    (route) => false,
-                  );
-                },
-
-
-                child:
-                    const Text(
-                  'بازگشت به فروشگاه',
-                ),
-              ),
+              PaymentResultActions(onViewOrder: () {
+                  Navigator.pushNamedAndRemoveUntil(context, AppRoutes.orderDetail, ModalRoute.withName(AppRoutes.home), arguments: order.id);
+                }, onBackToStore: () {
+                  Navigator.pushNamedAndRemoveUntil(context, AppRoutes.home, (route) => false);
+                }),
             ],
           ),
         ),
