0115d501f4fc0f6fd80345e61d29ee6beb5e29c2
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-22 00:06:02 +0330

    test(flutter): wrap app widget test in ProviderScope

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/main.dart';
 
 void main() {
   testWidgets('E-Commerce app loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const EcommerceApp());
+    await tester.pumpWidget(const ProviderScope(child: EcommerceApp()));
 
     expect(find.byType(EcommerceApp), findsOneWidget);
   });
